3c5a5c0bfcda7146dc6a8440bcc10dae925e0447
jabteepabuser <davekensk8@icloud.com>
2025-04-15 16:00:05 -0500

    add comments

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ const drills = [
   function generateDrill() {
     const randomIndex = Math.floor(Math.random() * drills.length);
     document.getElementById('drill-display').innerText = drills[randomIndex];
-  }
+  } //study functions, get element by ID, methods, math.random
   
