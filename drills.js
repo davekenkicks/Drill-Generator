04ba6ad6f1356f5c6b5fe0198e1786f91923d4ba
jabteepabuser <davekensk8@icloud.com>
2025-04-15 16:30:50 -0500

    added a separate drill bank

---
+++ drills.js
@@ -0,0 +1,9 @@
+const drills = [
+    "Jab - Cross - Hook",
+    "Lead Teep - Rear Round Kick",
+    "Check - Return Low Kick",
+    "Cross - Hook - Rear Kick",
+    "Jab - Cross - Switch Kick",
+    "Jab - Rear Elbow - Lead Knee",
+    "Feint Jab - Rear Roundhouse"
+];
--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-const drills = [
-    "Jab - Cross - Hook",
-    "Lead Teep - Rear Round Kick",
-    "Check - Return Low Kick",
-    "Cross - Hook - Rear Kick",
-    "Jab - Cross - Switch Kick",
-    "Jab - Rear Elbow - Lead Knee",
-    "Feint Jab - Rear Roundhouse"
-  ];
-  
-  function generateDrill() {
-    const randomIndex = Math.floor(Math.random() * drills.length);
-    document.getElementById('drill-display').innerText = drills[randomIndex];
-  } //study functions, get element by ID, methods, math.random
-  
+function generateDrill() {
+  const randomIndex = Math.floor(Math.random() * drills.length);
+  document.getElementById('drill-display').innerText = drills[randomIndex];
+} //study functions, get element by ID, methods, math.random
