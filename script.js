10f1da10052f6bb53ecffb8a974ea120aeaec3ce
jabteepabuser <davekensk8@icloud.com>
2025-04-15 22:49:22 -0500

    created list baby

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const drillDisplay = document.getElementById('drill-display');
 const generateDrillButton = document.getElementById('generate-drill');
+const listDisplay = document.getElementById('list-display'); //Select the list
+
+
 
 generateDrillButton.addEventListener('click', generateDrill);
 
 function generateDrill(){
-  const randomIndex = Math.floor(Math.random()*drills.length);
-  drillDisplay.innerText = drills[randomIndex];
+  const randomIndex = Math.floor(Math.random() * drills.length);
+  randomDrill = drills[randomIndex];
+  drillDisplay.innerText = randomDrill;
+
+  const createList = document.createElement('li'); //Create a new list item
+  createList.innerText = randomDrill; //Set the text content into the randomDrill
+  listDisplay.appendChild(createList); //append it to the list. 
+
 };
+
+
+
 
