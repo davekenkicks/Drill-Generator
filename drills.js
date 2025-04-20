95950df46253f6d2f179b60c4d9eabbcadca6971
jabteepabuser <davekensk8@icloud.com>
2025-04-20 12:04:06 -0500

    added new combo

--- drills.js
+++ drills.js
@@ -1,4 +1,5 @@
 const drills = [
     "Jab - Inside Leg Kick, Feint Jab - Inside Leg Kick, Feint Inside Leg Kick, (Opponnent does not react with Stance Switch.) Cross to Body - L. Hook - Low Kick.",
-    "Jab - Inside Leg Kick, Feint Jab - Inside Leg Kick, Feint Inside Leg Kick, (Opponnent Switch Stances) - Lead Hand Probe, Step to Outside - Rear Kick."
+    "Jab - Inside Leg Kick, Feint Jab - Inside Leg Kick, Feint Inside Leg Kick, (Opponnent Switch Stances) - Lead Hand Probe, Step to Outside - Rear Kick.",
+    "Jab - Left Hook 2x, Jab - Body Hook - Low Kick."
 ];
