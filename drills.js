f5c5a576a94d08071079f59ccf624365384211a6
jabteepabuser <davekensk8@icloud.com>
2025-04-22 13:31:35 -0500

    correction

--- drills.js
+++ drills.js
@@ -2,7 +2,7 @@ const drills = [
     "Jab - Inside Leg Kick, Feint Jab - Inside Leg Kick, Feint Inside Leg Kick, (Opponnent does not react with Stance Switch.) Cross to Body - L. Hook - Low Kick.",
     "Jab - Inside Leg Kick, Feint Jab - Inside Leg Kick, Feint Inside Leg Kick, (Opponnent Switch Stances) - Lead Hand Probe, Step to Outside - Rear Kick.",
     "Jab - Left Hook 2x, Jab - Body Hook - Low Kick.",
-    "Slip, Overhand Right, Switch Kick, Overhand Right.",
+    "Slip Jab, Overhand Right, Switch Kick, Overhand Right.",
     "Slip Jab, Evade Lead Head Kick, March Back Forward, Cross - Hook - Low Kick.",
     "Slip Jab, Parry Jab - Step To Right Angle, Cross - Left Hook - Low Kick."
 ];
